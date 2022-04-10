ee8a00a8036991b50c379e07ed4cf4d032321aab
Mario <s.mario95@googlemail.com>
2022-04-10 21:22:44 +0200

    Add some simple vscode interaction

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -8,7 +8,7 @@ export function activate(context: vscode.ExtensionContext) {
 			if (editor) {
 				const html = editor.document.getText(editor.selection);
 				if (html.length > 0) {
-					let params = ["translate"];
+					const params = ["translate"];
 					if (cmd.includes("Component")) params.push("--component");
 					params.push("--source");
 					params.push(html);
@@ -29,4 +29,34 @@ export function activate(context: vscode.ExtensionContext) {
 
 	registerCommand('extension.htmlToDioxusRsx');
 	registerCommand('extension.htmlToDioxusComponent');
+	context.subscriptions.push(vscode.commands.registerCommand('extension.addDioxusComponent', (...p) => AddDioxusComponent(p)));
+
+}
+
+function AddDioxusComponent(p: any[]) {
+	if (p && typeof p[0] == 'object' && p[0]['path']) {
+		const file: vscode.Uri = p[0];
+		vscode.workspace.fs.stat(file).then((fStat) => {
+			if (fStat.type == vscode.FileType.Directory) {
+				vscode.window.showInputBox({ title: 'Select a name' }).then((value) => {
+					if (value) {
+						const params: string[] = ["add", "component", file.fsPath, value];
+						const child_proc = spawn('dioxus', params);
+						let err_txt = '';
+						let success_txt = '';
+						child_proc.stderr.on('data', d => err_txt += d);
+						child_proc.stdout.on('data', d => success_txt += d);
+						child_proc.on('close', (code) => {
+							if (code !== 0) {
+								vscode.window.showErrorMessage(err_txt);
+								return;
+							} else {
+								vscode.window.showInformationMessage(success_txt);
+							}
+						});
+					}
+				});
+			}
+		});
+	}
 }
